43a6351465f7261c99e303ca1445028213b6a505
daniel_dl7 <danielvitorcg@gmail.com>
2022-03-31 13:44:13 -0300

    Documentação da API com redoc

--- Backend/setup/settings.py
+++ Backend/setup/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'escola',
     'corsheaders',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- Backend/setup/urls.py
+++ Backend/setup/urls.py
@@ -4,6 +4,23 @@ from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMa
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ESCOLA API",
+      default_version='v1',
+      description="API para gerenciamento de Alunos, Cursos e Matriculas",
+      terms_of_service="#",
+      contact=openapi.Contact(email="danielvitorcg@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
@@ -13,6 +30,7 @@ router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls) ),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
     path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
