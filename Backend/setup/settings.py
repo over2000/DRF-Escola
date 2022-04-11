bf1001391768a38a02bd59f1230a1ce3a2cde7ab
daniel_dl7 <danielvitorcg@gmail.com>
2022-04-11 12:44:22 -0300

    Novo modulo de autenticação

--- Backend/setup/settings.py
+++ Backend/setup/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'autenticacao',
     'escola',
     'corsheaders',
     'drf_yasg',
--- Backend/setup/urls.py
+++ Backend/setup/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
+from autenticacao.views import UserViewSet
 from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
+from rest_framework.authtoken import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -23,14 +25,16 @@ schema_view = get_schema_view(
 
 
 router = routers.DefaultRouter()
-router.register('alunos', AlunosViewSet, basename='Alunos')
-router.register('cursos', CursosViewSet, basename='Cursos')
-router.register('matriculas', MatriculaViewSet, basename='Matriculas')
+router.register(r'users', UserViewSet)
+router.register(r'alunos', AlunosViewSet, basename='Alunos')
+router.register(r'cursos', CursosViewSet, basename='Cursos')
+router.register(r'matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls) ),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
-    path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
+   path('', include(router.urls) ),
+   path('admin/', admin.site.urls),
+   path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
+   path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
